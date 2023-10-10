ae219886a54f6d0276513e7201d36db53e3a10cd
Burakcan Burcak <burakburcak72@gmail.com>
2023-10-10 11:30:29 +0200

    add cookie banner

--- script.js
+++ script.js
@@ -1,25 +1,33 @@
-/* Javascript to show and hide cookie banner using localstorage */
-/* Shows the Cookie banner */
-function showCookieBanner() {
-    let cookieBanner = document.getElementById("cb-cookie-banner");
-    cookieBanner.style.display = "block";
+// Fonction pour définir un cookie
+function setCookie(name, value, days) {
+    const expires = new Date(Date.now() + days * 24 * 60 * 60 * 1000).toUTCString();
+    document.cookie = `${name}=${value}; expires=${expires}; path=/`;
 }
 
-/* Hides the Cookie banner and saves the value to localstorage */
-function hideCookieBanner() {
-    localStorage.setItem("cb_isCookieAccepted", "yes");
-    let cookieBanner = document.getElementById("cb-cookie-banner");
-    cookieBanner.style.display = "none";
+// Fonction pour obtenir la valeur d'un cookie
+function getCookie(name) {
+    const cookies = document.cookie.split('; ');
+    for (const cookie of cookies) {
+        const [cookieName, cookieValue] = cookie.split('=');
+        if (cookieName === name) {
+            return cookieValue;
+        }
+    }
+    return null;
 }
 
-/* Checks the localstorage and shows Cookie banner based on it. */
-function initializeCookieBanner() {
-    let isCookieAccepted = localStorage.getItem("cb_isCookieAccepted");
-    if (isCookieAccepted === null) {
-        localStorage.setItem("cb_isCookieAccepted", "no");
-        showCookieBanner();
-    }
-    if (isCookieAccepted === "no") {
-        showCookieBanner();
-    }
+// Vérifie si le cookie d'acceptation existe
+const cookieAccepted = getCookie('cookieAccepted');
+
+// Si le cookie n'existe pas, affiche la bannière de cookie
+if (!cookieAccepted) {
+    const cookieBanner = document.getElementById('cookie-banner');
+    const acceptCookieButton = document.getElementById('accept-cookie');
+
+    cookieBanner.style.display = 'block';
+
+    acceptCookieButton.addEventListener('click', () => {
+        setCookie('cookieAccepted', 'true', 365); // Définit le cookie d'acceptation pour 1 an
+        cookieBanner.style.display = 'none';
+    });
 }
